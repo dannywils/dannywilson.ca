066e935512bab37139fc3c7915933991236c11bc
Danny Wilson <dwilson@getchalk.com>
2018-01-31 20:24:41 -0400

    Use gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,19 @@ module.exports = {
         name: 'images'
       }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: []
+        plugins: [
+          `gatsby-plugin-sharp`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
       }
     },
     {
@@ -32,6 +39,18 @@ module.exports = {
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Danny Wilson',
+        short_name: 'Danny Wilson',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui'
+      }
+    },
+    `gatsby-plugin-offline`
   ]
 };
