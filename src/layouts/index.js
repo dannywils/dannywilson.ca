c4c9a41838f4153a38b7fc5a7143c90425f3bd02
Danny Wilson <dwilson@getchalk.com>
2018-04-22 19:28:35 -0300

    Remove navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,7 +13,9 @@ import './all.sass';
 export default ({
   children,
   data: {
-    site: { siteMetadata: { title, subtitle } },
+    site: {
+      siteMetadata: { title, subtitle }
+    },
     allMarkdownRemark: { edges }
   }
 }) => (
@@ -25,35 +27,6 @@ export default ({
           <Link to="/">{title}</Link>
         </h1>
         <h2 className="subtitle">{subtitle}</h2>
-      </div>
-      <div className="column is-5 container is-paddingless">
-        <nav className="tabs is-boxed is-centered is-fullwidth">
-          <ul>
-            <li>
-              <Link to="/" exact activeClassName="is-active is-large">
-                <HomeIcon />&nbsp;Home
-              </Link>
-            </li>
-
-            <li>
-              <Link to="/about" exact activeClassName="is-active is-large">
-                <UserIcon />&nbsp;About
-              </Link>
-            </li>
-
-            <li>
-              <a href="https://www.linkedin.com/in/dannywils/">
-                <LinkedInIcon />&nbsp;LinkedIn
-              </a>
-            </li>
-
-            <li>
-              <a href="https://twitter.com/dannywils">
-                <TwitterIcon />&nbsp;Twitter
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </section>
     <div className="container">{children()}</div>
