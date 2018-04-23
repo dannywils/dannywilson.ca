dd4370cf51bcd44f3fd015de7e7517e4060584ae
Danny Wilson <dwilson@getchalk.com>
2018-04-22 23:02:09 -0300

    Remove netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,43 +7,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images'
-      }
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 900,
-              linkImagesToOriginal: false,
-              backgroundColor: 'transparent'
-            }
-          }
-        ]
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Danny Wilson',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,37 @@
 import React from 'react';
-import Link from 'gatsby-link';
-import Script from 'react-load-script';
 import graphql from 'graphql';
+import Helmet from 'react-helmet';
 
-import Article from '../components/Article';
+import './style.scss';
 
 export default class IndexPage extends React.Component {
-  handleScriptLoad() {
-    if (typeof window !== `undefined` && window.netlifyIdentity) {
-      window.netlifyIdentity.on('init', user => {
-        if (!user) {
-          window.netlifyIdentity.on('login', () => {
-            document.location.href = '/admin/';
-          });
-        }
-      });
-    }
-    window.netlifyIdentity.init();
-  }
-
   render() {
-    const { data: { allMarkdownRemark: { edges } } } = this.props;
+    const {
+      data: {
+        site: {
+          siteMetadata: { title, subtitle }
+        }
+      }
+    } = this.props;
 
     return (
       <React.Fragment>
-        <Script
-          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          onLoad={() => this.handleScriptLoad()}
-        />
-
-        <div className="column is-7 container">
-          {edges
-            .filter(post => post.node.frontmatter.templateKey === 'blog-post')
-            .map(
-              ({
-                node: {
-                  excerpt,
-                  id,
-                  frontmatter: { title, path, date, content }
-                }
-              }) => (
-                <Article
-                  key={id}
-                  id={id}
-                  title={<Link to={path}>{title}</Link>}
-                  subtitle={date}
-                  content={excerpt}
-                />
-              )
-            )}
-        </div>
+        <Helmet title={title} />
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <a href="mailto:danny@dannywilson.ca" title="Contact me">
+          📧
+        </a>
       </React.Fragment>
     );
   }
 }
-
 export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-            path
-          }
-        }
+  query pageQuery {
+    site {
+      siteMetadata {
+        title
+        subtitle
       }
     }
   }
